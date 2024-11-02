f7863e4f1d39e568d2f9db8902664d09a39058d2
HossamGouda <hos.gouda90@gmail.com>
2024-11-02 23:43:50 +0300

    update the app to use redux for state managment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 import App from "./container/App";
 import "./index.css";
+import {searchRobots} from "./reducers";
 import "tachyons";
+
+const store = createStore(searchRobots);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
